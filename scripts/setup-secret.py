1c679486b075fe06be84d724315a7453ce92f299
Rodrigo José Madeira Lourenço <rodrigo.jm.lourenco@gmail.com>
2016-03-01 14:50:22 +0000

    correccoes ao setup-secret.py

--- scripts/setup-secret.py
+++ scripts/setup-secret.py
@@ -2,6 +2,10 @@ import os
 import binascii
 
 secret_dir = os.environ['TRACE_DIR']+'/.secret/'
+
+if not os.path.exists(secret_dir):
+	os.makedirs(secret_dir)
+
 key = binascii.hexlify(os.urandom(32))
 secret = open(secret_dir+"key", 'w')
 secret.write(key)
