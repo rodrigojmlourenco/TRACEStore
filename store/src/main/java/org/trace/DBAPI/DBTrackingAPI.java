4b5fdfa678a69fa1dbd7d077a3662bfa7b099a0a
miguel <kostah50@gmail.com>
2016-03-29 15:40:42 +0100

    getSessionsDetails(sessions) done NOTE: This method only works with proper sessions, i.e., with sessions that contain "totalDistance", a start edge and a finish edge. TODO: Control this fact and make sure the query doesn't explode in case the session isn't proper.

--- store/src/main/java/org/trace/DBAPI/DBTrackingAPI.java
+++ store/src/main/java/org/trace/DBAPI/DBTrackingAPI.java
@@ -17,7 +17,7 @@ import org.trace.store.services.TRACEStoreService;
 import io.netty.util.internal.SystemPropertyUtil;
 
 public class DBTrackingAPI extends DBAPI{
-	
+
 	private final Logger LOG = Logger.getLogger(TRACEStoreService.class); 
 
 	public DBTrackingAPI(Client client){
@@ -41,7 +41,7 @@ public class DBTrackingAPI extends DBAPI{
 	}
 
 	private boolean login(String sessionID){
-		
+
 		if(sessionID == null){
 			LOG.error("login: sessionID is null!");
 			return false;
@@ -58,8 +58,8 @@ public class DBTrackingAPI extends DBAPI{
 		//begin a session
 		results = query("g.V().has('sessionID', sessionID).hasNext();"
 				+ "", params);
-		
-//		LOG.info("login: session exists? " + results.get(0).getBoolean());
+
+		//		LOG.info("login: session exists? " + results.get(0).getBoolean());
 
 		if(!results.get(0).getBoolean()){
 			//begin a session
@@ -67,8 +67,8 @@ public class DBTrackingAPI extends DBAPI{
 			results = query("S = graph.addVertex(label,'session','sessionID', sessionID, 'date', date);"
 					+ "", params);
 		}
-		
-//		LOG.info("login: results != null:" + (results != null));
+
+		//		LOG.info("login: results != null:" + (results != null));
 
 		return results != null;
 	}
@@ -545,6 +545,73 @@ public class DBTrackingAPI extends DBAPI{
 		}
 	}
 
+	public List<String> getSessionsDetails(List<String> sessions){
+		//return list
+		List<String> sessionsDetails = new ArrayList<>();
+		
+		//Nothing to process
+		if(sessions == null || sessions.isEmpty()){
+			LOG.error("getSessionsDetails: provided list of sessions is null or empty");
+			return sessionsDetails;
+		}
+
+		//return values list
+		List<Result> results = null;
+				
+		//params
+		Map<String,Object> params = new HashMap<>();
+		
+		//String with the desired query
+		String queryString = "";
+		
+		queryString = "A = new ArrayList<>();";
+		
+		//For each given session
+		int i = 0;
+		for(String s : sessions){
+			//Add the corresponding ID in the params
+			params.put("sessionID" + i, s); 
+			
+			//Find the corresponding session vertex
+			queryString += "S"+i+" = g.V().has('sessionID',sessionID"+i+");";
+			
+			//Add its totalDistance to the returning list
+			queryString += "A.add(S"+i+".clone().values('totalDistance').next());";
+//			
+//			//Get the STARTING POINT
+			queryString += "Start"+i+"= S"+i+".clone().outE().inV().values('location').next().getPoint();";
+//			
+//			//Add its Lat
+			queryString += "A.add(Start"+i+".getLatitude());";
+//			//Add its Lon
+			queryString += "A.add(Start"+i+".getLongitude());";
+//
+//			//Get the FINISHING POINT
+			queryString += "Finish"+i+"= S"+i+".clone().inE().outV().values('location').next().getPoint();";
+//			
+//			//Add its Lat
+			queryString += "A.add(Finish"+i+".getLatitude());";
+//			//Add its Lon
+			queryString += "A.add(Finish"+i+".getLongitude());";
+			
+			i++;
+		}
+		queryString += "A;";
+		
+		results = query(queryString,params);
+		
+		if(results == null || results.isEmpty()){
+			LOG.error("getSessionsDetails: Results from the query are either null or empty.");
+			return sessionsDetails;
+		}
+		
+		for(Result r : results){
+			sessionsDetails.add(r.getString());
+		}
+
+		return sessionsDetails;
+	}
+
 
 	public String getSessionDetails(String sessionID){
 		//return values list
@@ -696,7 +763,7 @@ public class DBTrackingAPI extends DBAPI{
 
 		return userSessions;
 	}
-	
+
 	private boolean sessionIsEmpty(String sessionID){
 		if(sessionID == null){
 			LOG.error("sessionIsEmpty: sessionID is null!");
@@ -716,30 +783,30 @@ public class DBTrackingAPI extends DBAPI{
 				+ "B = true;"
 				//Check if it actually exists
 				+ "if(A.clone().hasNext()){"
-					//In case that it does, check if it has any route already set to it
+				//In case that it does, check if it has any route already set to it
 				+ 	"B = A.outE().has('type','start').hasNext();"
 				+ 	"B = !B;"
 				+ "}else{"
-					//In case it doesn't exist, create one.
+				//In case it doesn't exist, create one.
 				+ 	"graph.addVertex(label,'session','sessionID', sessionID, 'date', date);"
 				+ "};"
 				+ "B;"
 				+ "", params);
-		
-//		if(results != null){
-//			LOG.info("results != null:true");
-//			LOG.info("results.get(0).getBoolean():" + results.get(0).getBoolean());
-//		}
-		
+
+		//		if(results != null){
+		//			LOG.info("results != null:true");
+		//			LOG.info("results.get(0).getBoolean():" + results.get(0).getBoolean());
+		//		}
+
 		return ((results != null) && results.get(0).getBoolean());
 	}
-	
+
 	public boolean submitRoute(String sessionID, List<TraceVertex> submittedRoute){
 		boolean success = true;
-		
+
 		//Route parsing so that there are no two subsequent points being added with the same coords.
 		List<TraceVertex> route = TraceLocationMethods.routeParser(submittedRoute);
-		
+
 		LOG.info("submitRoute: submittedRoute:" + submittedRoute.size());
 		LOG.info("submitRoute: route:" + route.size());
 
@@ -747,7 +814,9 @@ public class DBTrackingAPI extends DBAPI{
 			LOG.error("submitRoute: session is not empty!");
 			return false;
 		}
-		
+
+		Double totalDistance = TraceLocationMethods.routeTotalDistance(route);
+
 		//TODO: split the route in chunks of 80
 		boolean firstOne = true;
 		int i = 0;
@@ -755,7 +824,7 @@ public class DBTrackingAPI extends DBAPI{
 			LOG.info("submitRoute: i:" + i);
 			if(firstOne){
 				firstOne = false;
-				success = submitFirstRoute(sessionID,route.subList(i, i+80)) && success;
+				success = submitFirstRoute(sessionID,route.subList(i, i+80), totalDistance) && success;
 			}else{
 				success = submitMoreRoutes(sessionID, route.subList(i, i+80)) && success;
 			}
@@ -764,17 +833,17 @@ public class DBTrackingAPI extends DBAPI{
 		if(route.size() > i){
 			if(firstOne){
 				firstOne = false;
-				success = submitFirstRoute(sessionID, route.subList(i, route.size())) && success;
+				success = submitFirstRoute(sessionID, route.subList(i, route.size()), totalDistance) && success;
 			}else{
 				success = submitMoreRoutes(sessionID, route.subList(i, route.size())) && success;
 			}
-//			success = submitMoreRoutes(sessionID, route.subList(i, route.size())) && success;
+			//			success = submitMoreRoutes(sessionID, route.subList(i, route.size())) && success;
 		}
-		
+
 		return success;
 	}
-	
-	private boolean submitFirstRoute(String sessionID, List<TraceVertex> route){
+
+	private boolean submitFirstRoute(String sessionID, List<TraceVertex> route, double totalDistance){
 		boolean success = true;
 
 		//Verify there's actually a sessionID and that we can associate this route with it
@@ -782,7 +851,7 @@ public class DBTrackingAPI extends DBAPI{
 			LOG.error("submitFirstRoute: The login failed");
 			return false;
 		}
-		
+
 		if(route == null || route.isEmpty()){
 			LOG.error("submitFirstRoute: route is null or empty");
 			return false;
@@ -797,11 +866,13 @@ public class DBTrackingAPI extends DBAPI{
 		//params
 		Map<String,Object> params = new HashMap<>();
 		params.put("sessionID", sessionID);
+		params.put("totalDistance", totalDistance);
+
 		params.put("SWLat", routeBoundingBox.getSWLat());
 		params.put("SWLon", routeBoundingBox.getSWLon());
 		params.put("NELat", routeBoundingBox.getNELat());
 		params.put("NELon", routeBoundingBox.getNELon());
-		
+
 		params.put("latitude", route.get(0).getLatitude());
 		params.put("longitude", route.get(0).getLongitude());
 		params.put("vertexID", ""+route.get(0).getLatitude()+"_"+route.get(0).getLongitude());
@@ -816,22 +887,22 @@ public class DBTrackingAPI extends DBAPI{
 
 		//Set A as the sub collection of vertices that we will need for this route (removed this: filter{it.get().label() != 'session'})
 		queryString += "A = g.V().has('location', geoWithin(Geoshape.box(SWLat,SWLon,NELat,NELon))).outE();";
-		
+
 		//Set subGraph as the graph that only contains those vertices
 		queryString += "subGraph = A.subgraph('subGraph').cap('subGraph').next();";
-		
+
 		//Set the traversal graph
 		queryString += "sg = subGraph.traversal(standard());";
-		
+
 		//Set A
 		queryString += "A = sg.V();";
-		
+
 		//Set P0 as the route point we are considering now.
 		queryString += "P0 = Geoshape.point(latitude,longitude);";
-		
+
 		//Filter based on the gpsToleranceolerance and order the vertices based on proximity to the vertex we are considering right now, i.e., closest as the first of the collection.
 		queryString += "B0 = A.clone().has('location', geoWithin(Geoshape.circle(latitude, longitude, gpsTolerance))).as('a').map{it.get().value('location').getPoint().distance(P0.getPoint())}.order().by(incr).select('a');";
-		
+
 		//Check if the list is not empty. Check if the 1st entry is within the acceptable gps tolerance. 
 		//In case it's acceptable consider this point
 		//In case the point is not acceptable, add a new point to the DB and also to the initial "A" collection.
@@ -840,54 +911,56 @@ public class DBTrackingAPI extends DBAPI{
 				+ "if(Aux0.hasNext()){C0 = Aux0.next().id();}else{C0 = graph.addVertex(label,'unmapped_location','vertexID', vertexID,'location', P0).id();};"
 				+ "};";
 
+		queryString += "S.property('totalDistance',totalDistance);";
+
 		//Connect the session vertex to the first point of the route.
 		queryString += "S.addEdge('session', g.V(C0).next(), 'type', 'start', 'sessionID', sessionID, 'date', date0);";
 
 		//Now "kinda" repeat for every point of the route
 		for(int i = 1; i < route.size(); i++){
-//			//First get the latitude, longitude, possible vertexID and date for each point of the route
+			//			//First get the latitude, longitude, possible vertexID and date for each point of the route
 			params.put("lat"+i, route.get(i).getLatitude());
 			params.put("lon"+i, route.get(i).getLongitude());
 			params.put("vertexID"+i, "" + route.get(i).getLatitude() + "_" + route.get(i).getLongitude());
 			params.put("date"+i, route.get(i).getDate());
-//
-//			//Set Pi as the route point we are considering now.
+			//
+			//			//Set Pi as the route point we are considering now.
 			queryString += "P"+i+" = Geoshape.point(lat"+i+",lon"+i+");";
-//
-//			//Clone A and order the vertices based on the proximity to Pi
+			//
+			//			//Clone A and order the vertices based on the proximity to Pi
 			queryString += "B"+i+ " = A.clone().has('location', geoWithin(Geoshape.circle(lat"+i+", lon"+i+", gpsTolerance))).as('a').map{it.get().value('location').getPoint().distance(P"+i+".getPoint())}.order().by(incr).select('a');";
-//
-//			//Check if the list is not empty. Check if the 1st entry is within the acceptable gps tolerance. 
-//			//In case it's acceptable consider this point
-//			//In case the point is not acceptable, add a new point to the DB and also to the initial "A" collection.
+			//
+			//			//Check if the list is not empty. Check if the 1st entry is within the acceptable gps tolerance. 
+			//			//In case it's acceptable consider this point
+			//			//In case the point is not acceptable, add a new point to the DB and also to the initial "A" collection.
 			queryString += "if(B"+i+".hasNext()){C"+i+" = B"+i+".next().id();}else{"
 					+ "Aux"+i+" = g.V().has('vertexID',vertexID"+i+");"
 					+ "if(Aux"+i+".hasNext()){C"+i+" = Aux"+i+".next().id();}else{C"+i+" = graph.addVertex(label,'unmapped_location','vertexID', vertexID"+i+",'location', P"+i+").id();};"
 					+ "};";
-//
-//			//Connect the session vertex to the first point of the route.
+			//
+			//			//Connect the session vertex to the first point of the route.
 			queryString += "g.V(C"+(i-1)+").next().addEdge('session', g.V(C"+i+").next(), 'type', 'trajectory', 'sessionID', sessionID, 'date', date"+i+");";
 		}
-//
-//		//Complete the cycle and close the route with the "finish" edge
+		//
+		//		//Complete the cycle and close the route with the "finish" edge
 		queryString += "g.V(C"+(route.size()-1)+").next().addEdge('session', S, 'type', 'finish', 'sessionID', sessionID, 'date', date"+(route.size()-1)+");";
 
 		LOG.info("submitFirstRoute: route length:" + route.size());
 		LOG.info("submitFirstRoute: queryString has a length of:" + queryString.length());
-		
+
 		results = query(queryString,params);
 
 		return results!=null;
 	}
-	
+
 	private boolean submitMoreRoutes(String sessionID, List<TraceVertex> route){
 		boolean success = true;
-		
+
 		if(sessionIsEmpty(sessionID)){
 			LOG.error("submitMoreRoutes: Session is empty, this method expects at least one route submitted before");
 			return false;
 		}
-		
+
 		if(route == null || route.isEmpty()){
 			LOG.error("submitMoreRoutes: route is null or empty");
 			return false;
@@ -906,7 +979,7 @@ public class DBTrackingAPI extends DBAPI{
 		params.put("SWLon", routeBoundingBox.getSWLon());
 		params.put("NELat", routeBoundingBox.getNELat());
 		params.put("NELon", routeBoundingBox.getNELon());
-		
+
 		params.put("latitude", route.get(0).getLatitude());
 		params.put("longitude", route.get(0).getLongitude());
 		params.put("vertexID", ""+route.get(0).getLatitude()+"_"+route.get(0).getLongitude());
@@ -915,34 +988,34 @@ public class DBTrackingAPI extends DBAPI{
 
 		//String that will be submitted for the query
 		String queryString = "";
-		
+
 		//Get the session "finish" edge
 		queryString += "E0 = g.V().hasLabel('session').has('sessionID', sessionID).inE().has('type','finish');";
-		
+
 		//Get the ID of the last vertex
 		queryString += "finishID = E0.clone().next().id();";
 
 		//Get the Vertex of the last location of the session
 		queryString += "S = E0.clone().outV().next();";
-		
+
 		//Set A as the sub collection of vertices that we will need for this route (removed this: filter{it.get().label() != 'session'})
 		queryString += "A = g.V().has('location', geoWithin(Geoshape.box(SWLat,SWLon,NELat,NELon))).outE();";
-		
+
 		//Set subGraph as the graph that only contains those vertices
 		queryString += "subGraph = A.subgraph('subGraph').cap('subGraph').next();";
-		
+
 		//Set the traversal graph
 		queryString += "sg = subGraph.traversal(standard());";
-		
+
 		//Set A
 		queryString += "A = sg.V();";
-		
+
 		//Set P0 as the route point we are considering now.
 		queryString += "P0 = Geoshape.point(latitude,longitude);";
-		
+
 		//Filter based on the gpsToleranceolerance and order the vertices based on proximity to the vertex we are considering right now, i.e., closest as the first of the collection.
 		queryString += "B0 = A.clone().has('location', geoWithin(Geoshape.circle(latitude, longitude, gpsTolerance))).as('a').map{it.get().value('location').getPoint().distance(P0.getPoint())}.order().by(incr).select('a');";
-		
+
 		//Check if the list is not empty. Check if the 1st entry is within the acceptable gps tolerance. 
 		//In case it's acceptable consider this point
 		//In case the point is not acceptable, add a new point to the DB and also to the initial "A" collection.
@@ -956,53 +1029,53 @@ public class DBTrackingAPI extends DBAPI{
 
 		//Now "kinda" repeat for every point of the route
 		for(int i = 1; i < route.size(); i++){
-//			//First get the latitude, longitude, possible vertexID and date for each point of the route
+			//			//First get the latitude, longitude, possible vertexID and date for each point of the route
 			params.put("lat"+i, route.get(i).getLatitude());
 			params.put("lon"+i, route.get(i).getLongitude());
 			params.put("vertexID"+i, "" + route.get(i).getLatitude() + "_" + route.get(i).getLongitude());
 			params.put("date"+i, route.get(i).getDate());
-//
-//			//Set Pi as the route point we are considering now.
+			//
+			//			//Set Pi as the route point we are considering now.
 			queryString += "P"+i+" = Geoshape.point(lat"+i+",lon"+i+");";
-//
-//			//Clone A and order the vertices based on the proximity to Pi
+			//
+			//			//Clone A and order the vertices based on the proximity to Pi
 			queryString += "B"+i+ " = A.clone().has('location', geoWithin(Geoshape.circle(lat"+i+", lon"+i+", gpsTolerance))).as('a').map{it.get().value('location').getPoint().distance(P"+i+".getPoint())}.order().by(incr).select('a');";
-//
-//			//Check if the list is not empty. Check if the 1st entry is within the acceptable gps tolerance. 
-//			//In case it's acceptable consider this point
-//			//In case the point is not acceptable, add a new point to the DB and also to the initial "A" collection.
+			//
+			//			//Check if the list is not empty. Check if the 1st entry is within the acceptable gps tolerance. 
+			//			//In case it's acceptable consider this point
+			//			//In case the point is not acceptable, add a new point to the DB and also to the initial "A" collection.
 			queryString += "if(B"+i+".hasNext()){C"+i+" = B"+i+".next().id();}else{"
 					+ "Aux"+i+" = g.V().has('vertexID',vertexID"+i+");"
 					+ "if(Aux"+i+".hasNext()){C"+i+" = Aux"+i+".next().id();}else{C"+i+" = graph.addVertex(label,'unmapped_location','vertexID', vertexID"+i+",'location', P"+i+").id();};"
 					+ "};";
-//
-//			//Connect the session vertex to the first point of the route.
+			//
+			//			//Connect the session vertex to the first point of the route.
 			queryString += "g.V(C"+(i-1)+").next().addEdge('session', g.V(C"+i+").next(), 'type', 'trajectory', 'sessionID', sessionID, 'date', date"+i+");";
 		}
 
-//		//Complete the cycle and close the route with the "finish" edge
+		//		//Complete the cycle and close the route with the "finish" edge
 		queryString += "g.V(C"+(route.size()-1)+").next().addEdge('session', S, 'type', 'finish', 'sessionID', sessionID, 'date', date"+(route.size()-1)+");";
-		
-//		queryString += "finishID;";
-		
+
+		//		queryString += "finishID;";
+
 		//Drop the session "finish" edge
 		queryString += "g.E(finishID).drop();";
-				
+
 		LOG.info("submitMoreRoutes: route length:" + route.size());
 		LOG.info("submitMoreRoutes: queryString has a length of:" + queryString.length());
-		
+
 		results = query(queryString,params);
-		
+
 		LOG.info("!results.isEmpty(): " + !results.isEmpty());
-//		
-//		params.clear();
-//		if(!results.isEmpty()){
-//			LOG.info("finishID: " + results.get(0).getString());
-//			params.put("finishID", results.get(0).getString());
-//
-//			queryString = "g.E(finishID).drop()";
-//			results = query(queryString,params);
-//		}
+		//		
+		//		params.clear();
+		//		if(!results.isEmpty()){
+		//			LOG.info("finishID: " + results.get(0).getString());
+		//			params.put("finishID", results.get(0).getString());
+		//
+		//			queryString = "g.E(finishID).drop()";
+		//			results = query(queryString,params);
+		//		}
 
 		return results!=null;
 	}
