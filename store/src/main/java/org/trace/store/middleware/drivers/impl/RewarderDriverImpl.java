d9bc692164539291592a7c19db8ef4de2c7165ae
miguel <kostah50@gmail.com>
2016-10-03 16:02:31 +0100

    zoom

--- store/src/main/java/org/trace/store/middleware/drivers/impl/RewarderDriverImpl.java
+++ store/src/main/java/org/trace/store/middleware/drivers/impl/RewarderDriverImpl.java
@@ -613,7 +613,7 @@ public class RewarderDriverImpl implements RewarderDriver {
 				+ latitude
 				+ ","
 				+ longitude
-				+ "&zoom=14&size=100x200&markers=color:red%7C"
+				+ "&zoom=15&size=100x200&markers=color:red%7C"
 				+ latitude
 				+ ","
 				+ longitude
